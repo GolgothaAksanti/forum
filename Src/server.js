cba668fb1fc36869b539ebe1a9aab0aadcc5ac41
Golgotha Aksanti <38277247+GolgothaAksanti@users.noreply.github.com>
2021-04-23 18:37:05 +0300

    Delete Src directory

--- Src/server.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import app from './app/app';
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  process.stdout.write(`server listening on: http://localhost:${PORT}`);
-});
